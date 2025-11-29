eae081da9040b87bc86d10cfd936c010e8742aa8
Abdulkaium007 <smakaium6957@gmail.com>
2025-11-30 04:36:26 +0600

    icons

--- src/components/AboutUs/TechStack.jsx
+++ src/components/AboutUs/TechStack.jsx
@@ -1,5 +1,7 @@
-import { FaReact, FaNodeJs } from "react-icons/fa";
-import { SiMysql, SiTailwindcss } from "react-icons/si";
+import { FaReact, FaNodeJs, FaFigma, FaGitAlt, FaGithub } from "react-icons/fa";
+import { SiMysql, SiTailwindcss, SiMongodb, SiMongoose, SiExpress, SiEjs, SiRedux, SiJavascript, SiPython, SiDjango, SiArduino } from "react-icons/si";
+import { BiLogoPostgresql } from "react-icons/bi";
+import { RiNextjsFill } from "react-icons/ri";
 
 const TechStack = () => {
   return (
@@ -11,6 +13,22 @@ const TechStack = () => {
         <FaNodeJs title="Node.js" className="hover:opacity-100 transition" />
         <SiMysql title="MySQL" className="hover:opacity-100 transition" />
         <SiTailwindcss title="TailwindCSS" className="hover:opacity-100 transition" />
+        <BiLogoPostgresql title="PostgreSQL" className="hover:opacity-100 transition" />
+        <SiMongodb title="MongoDB" className="hover:opacity-100 transition" />
+        <SiMongoose title="Mongoose" className="hover:opacity-100 transition" />
+        <SiExpress title="Express.js" className="hover:opacity-100 transition" />
+        <SiEjs title="EJS" className="hover:opacity-100 transition" />      
+        <RiNextjsFill title="Next.js" className="hover:opacity-100 transition" />
+        <FaFigma title="Figma" className="hover:opacity-100 transition" />
+        <SiRedux title="Redux" className="hover:opacity-100 transition" />
+        <SiJavascript title="JavaScript" className="hover:opacity-100 transition" />
+        <SiPython title="Python" className="hover:opacity-100 transition" />
+        <SiDjango title="Django" className="hover:opacity-100 transition" />
+        <SiArduino title="Arduino" className="hover:opacity-100 transition" />
+        <FaGitAlt title="Git" className="hover:opacity-100 transition" />
+        <FaGithub title="Github" className="hover:opacity-100 transition" />
+
+
       </div>
     </section>
   );
